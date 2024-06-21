83a545c9181aaed9b0f173fcd9d304a56cf98f07
unknown <saummanzoor49@gmail.com>
2024-06-21 19:47:12 +0500

    histogram__colored_img

---
+++ histogram_color.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv.imread('./images/img2.jpeg')
+img = cv.resize(img,(400,400))
+
+# creating mask
+blank = np.zeros(img.shape[:2],dtype='uint8') 
+
+circle = cv.circle(blank,(img.shape[1]//2,img.shape[0]//2),100,255,-1)
+
+# mask = cv.bitwise_and(img,img,mask=circle)
+
+# creating histogram
+colors = ('b','g','r')
+
+plt.figure()
+plt.title('Hist')
+plt.xlabel('Bins')
+plt.ylabel('Pxs')
+
+for i,col in enumerate(colors): 
+    hist = cv.calcHist([img],[i],circle,[256],[0,256])
+    plt.plot(hist,color=col)
+    plt.xlim([0,256])
+
+plt.show()
+
+
+
+
+cv.waitKey(0)
+
+
